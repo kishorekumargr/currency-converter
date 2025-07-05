c90a5b713615960f72529642afb1a2cd4186bd99
Kishore <kishorekumar1100011@gmail.com>
2025-07-05 11:00:26 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,163 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect, ChangeEvent } from "react";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+import ClipLoader from "react-spinners/ClipLoader";
+
+type ExchangeRates = {
+  [key: string]: number;
+};
+
+type Currency = "USD" | "INR" | "EUR" | "GBP" | "JPY" | "AUD" | "CAD" | "PKR";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [amount, setAmount] = useState<number | null>(null);
+  const [sourceCurrency, setSourceCurrency] = useState<Currency>("USD");
+  const [targetCurrency, setTargetCurrency] = useState<Currency>("INR");
+  const [exchangeRates, setExchangeRates] = useState<ExchangeRates>({});
+  const [convertedAmount, setConvertedAmount] = useState<string>("0.00");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  useEffect(() => {
+    const fetchExchangeRates = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch("https://open.er-api.com/v6/latest/USD");
+        const data = await response.json();
+        setExchangeRates(data.rates);
+      } catch (error) {
+        setError("Error fetching exchange rates.");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchExchangeRates();
+  }, []);
+
+  const handleAmountChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setAmount(parseFloat(e.target.value));
+  };
+
+  const handleSourceCurrencyChange = (value: Currency): void => {
+    setSourceCurrency(value);
+  };
+
+  const handleTargetCurrencyChange = (value: Currency): void => {
+    setTargetCurrency(value);
+  };
+
+  const calculateConvertedAmount = (): void => {
+    if (sourceCurrency && targetCurrency && amount && exchangeRates) {
+      const rate =
+        sourceCurrency === "USD"
+          ? exchangeRates[targetCurrency]
+          : exchangeRates[targetCurrency] / exchangeRates[sourceCurrency];
+      const result = amount * rate;
+      setConvertedAmount(result.toFixed(2));
+    }
+  };
+
+  return (
+    <main className="flex flex-col justify-center items-center bg-linear-to-t from-sky-500 to-indigo-500 px-4 h-screen">
+      <h1 className="mb-2 font-semibold text-white text-3xl md:text-4xl text-center">
+        Currency Converter
+      </h1>
+      <p className="mb-12 font-normal text-gray-100 text-base md:text-xl leading-[1.8rem]">
+        Check live foreign currency exchange rates
+      </p>
+      <Card className="space-y-4 p-6 rounded-3xl w-full max-w-3xl">
+        <CardContent>
+          {loading ? (
+            <div className="flex justify-center">
+              <ClipLoader className="w-6 h-6 text-blue-500" />
+            </div>
+          ) : error ? (
+            <div className="text-red-500 text-center">{error}</div>
+          ) : (
+            <div className="gap-4 grid">
+              <div className="items-center gap-2 grid grid-cols-[1fr_auto]">
+                <Label htmlFor="from" className="text-lg">
+                  From
+                </Label>
+                <div className="items-center gap-2 grid grid-cols-[1fr_auto]">
+                  <Input
+                    type="number"
+                    placeholder="Amount"
+                    value={amount || ""}
+                    onChange={handleAmountChange}
+                    className="w-full"
+                    id="from"
+                  />
+                  <Select value={sourceCurrency} onValueChange={handleSourceCurrencyChange}>
+                    <SelectTrigger className="w-24">
+                      <SelectValue placeholder="USD" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem value="USD">USD</SelectItem>
+                        <SelectItem value="INR">INR</SelectItem>
+                        <SelectItem value="EUR">EUR</SelectItem>
+                        <SelectItem value="GBP">GBP</SelectItem>
+                        <SelectItem value="JPY">JPY</SelectItem>
+                        <SelectItem value="AUD">AUD</SelectItem>
+                        <SelectItem value="CAD">CAD</SelectItem>
+                        <SelectItem value="PKR">PKR</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+              <div className="items-center gap-2 grid grid-cols-[1fr_auto]">
+                <Label htmlFor="to" className="text-lg">
+                  To
+                </Label>
+                <div className="items-center gap-2 grid grid-cols-[1fr_auto]">
+                  <div className="font-bold text-2xl">{convertedAmount}</div>
+                  <Select value={targetCurrency} onValueChange={handleTargetCurrencyChange}>
+                    <SelectTrigger className="w-24">
+                      <SelectValue placeholder="EUR" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem value="USD">USD</SelectItem>
+                        <SelectItem value="INR">INR</SelectItem>
+                        <SelectItem value="EUR">EUR</SelectItem>
+                        <SelectItem value="GBP">GBP</SelectItem>
+                        <SelectItem value="JPY">JPY</SelectItem>
+                        <SelectItem value="AUD">AUD</SelectItem>
+                        <SelectItem value="CAD">CAD</SelectItem>
+                        <SelectItem value="PKR">PKR</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            </div>
+          )}
+        </CardContent>
+        <CardFooter>
+          <Button
+            type="button"
+            className="bg-blue-500 hover:bg-[#207FE6] px-6 py-3 rounded-lg w-full h-fit font-semibold text-white text-base transition-colors duration-200"
+            onClick={calculateConvertedAmount}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            Convert
+          </Button>
+        </CardFooter>
+      </Card>
+    </main>
   );
 }
